dd084dbd4979febeb48274e731b5b751115e8e1c
HarishTeens <arishh2@gmail.com>
2024-03-08 17:05:53 +0530

    implement connection pooling on todos API

--- go.mod
+++ go.mod
@@ -6,9 +6,16 @@ require github.com/gin-gonic/gin v1.9.1
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.2 // indirect
 	github.com/jackc/pgx/v5 v5.5.4 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jackc/pgx/v5/pgtype"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,6 +19,9 @@ type Todo struct {
 	ID  int64 `gorm:"primaryKey;type:autoIncrement"`
 	Todo string
 	User_Id int64
+	CreatedAt pgtype.Date
+	DeletedAt pgtype.Date
+	UpdatedAt pgtype.Date
 }
 func (u *Todo) TableName() string {
     // custom table name, this is default
@@ -35,14 +41,14 @@ func (u *User) TableName() string {
 func main() {
 	r := gin.Default()
 	dsn := "host=localhost user=postgres password=postgres dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	pool, err := pgxpool.Connect(context.Background(), dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "Pong",
 		})
 	})
 
@@ -59,15 +65,34 @@ func main() {
 
 	r.GET("/todos/:id", func(c *gin.Context) {
 		user_id := c.Param("id")
-		todos := []Todo{}
-		err := db.Find(&todos, "user_id = ?", user_id).Error
+		ress, errrr := pool.Query(context.Background(), "SELECT * FROM todos where user_id = $1", user_id)
+		if errrr != nil {
+			log.Fatal(errrr)
+		}
+		defer ress.Close()
+		var rowss []Todo
+		for ress.Next() {
+			var b Todo
+			err := ress.Scan(
+				&b.ID,
+				&b.Todo,
+				&b.User_Id,
+				&b.CreatedAt,
+				&b.DeletedAt,
+				&b.UpdatedAt,
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rowss = append(rowss, b)
+		}
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"todos": todos,
+			"todos": rowss,
 			"user_id": user_id,
 		})
 	})
@@ -77,7 +102,6 @@ func main() {
 
 		userRecord := User{}
 		err := db.First(&userRecord,  "id = ?", user_id).Error
-
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error_message": "User not found",
