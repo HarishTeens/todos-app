a2c40a091911366a9805985d3048e76926d3dde9
HarishTeens <arishh2@gmail.com>
2024-03-10 00:32:20 +0530

    use go routines

--- controller.go
+++ controller.go
@@ -30,35 +30,65 @@ func addUser(c *gin.Context) {
 }
 
 func getTodo(c *gin.Context) {
-	user_id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	userID, _ := strconv.ParseInt(c.Param("id"), 10, 64)
 
-	user, _ := getUserById(pool, int(user_id))
-	if user.ID == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error_message": fmt.Sprintf("user with id %v not found", user_id),
-		})
-		return
-	}
-	
-	rowss, err := getTodosByUserId(pool, int(user_id))
-	if err != nil {
-		fmt.Println(err)
+	// Create channels for communication between goroutines
+	userCh := make(chan User)
+	errCh := make(chan error)
+
+	go func ()  {
+		user, _ := getUserByID(pool, int(userID))
+		if user.ID == 0 {
+			errCh <- fmt.Errorf("user with id %v not found", userID)
+			return
+		}
+		userCh <- user
+	}()
+
+	// Wait for results from goroutines
+	var user User
+	select {
+		case user = <-userCh:
+		case err := <-errCh:
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error_message": err,
+			})
+			return
+		}
+
+	// Create channels for communication between goroutines
+	todosCh := make(chan []Todo)
+
+	// Fetch todos concurrently
+	go func() {
+		todos, err := getTodosByUserID(pool, int(user.ID))
+		if err != nil {
+			errCh <- err
+			return
+		}
+		todosCh <- todos
+	}()
+
+	// Wait for results from goroutines
+	var todos []Todo
+	select {
+	case todos = <-todosCh:
+	case err := <-errCh:
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error_message": fmt.Sprintf("%v", err),
 		})
-		return			
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"todos": rowss,
-		"user_id": user_id,
+		"todos":   todos,
+		"user_id": userID,
 	})
-
 }
 
 func addTodoToUser(c *gin.Context) {
 	user_id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
-	user, _ := getUserById(pool, int(user_id))
+	user, _ := getUserByID(pool, int(user_id))
 	if user.ID == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error_message": fmt.Sprintf("user with id %v not found", user_id),
