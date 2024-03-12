6dbe8329bf87b8ba9e1158d04ed82d52cfea5d18
HarishTeens <arishh2@gmail.com>
2024-03-12 11:54:41 +0530

    use fast http client

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,8 @@
-from locust import HttpUser, task
+from locust import FastHttpUser, task
 import random
 
-class HelloWorldUser(HttpUser):
+class HelloWorldUser(FastHttpUser):
     @task
     def hello_world(self):
-        randuser = round(random.randint(1, 42000))
+        randuser = round(random.randint(1, 52000))
         self.client.get(f"/todos/{randuser}")
